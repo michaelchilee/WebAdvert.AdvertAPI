a54b85872315165323eef3ec8be0c6cc317af5dd
michaelchilee <54333755+michaelchilee@users.noreply.github.com>
2019-09-12 18:24:51 -0700

    Add files via upload

---
+++ AdvertAPI/Services/AdvertProfile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AdvertAPI.Models;
+using AutoMapper;
+
+namespace AdvertAPI.Services
+{
+    public class AdvertProfile : Profile
+    {
+        public AdvertProfile()
+        {
+            CreateMap<AdvertModel, AdvertDBModel>(); // will map API to DB by attribute names. As long as same, it will map them
+        }
+    }
+}
---
+++ AdvertAPI/Services/IAdvertStorageService.cs
@@ -0,0 +1,16 @@
+﻿using AdvertAPI.Models;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdvertAPI.Services
+{
+    public interface IAdvertStorageService
+    {
+        Task<string> Add(AdvertModel model);
+        Task Confirm(ConfirmAdvertModel model);
+    }
+}
